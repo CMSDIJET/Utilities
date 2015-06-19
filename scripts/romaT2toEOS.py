90e8b74ed4668cdcfabaedd91ec2a1a73677f562
Francesco Santanastasio <francesco.santanastasio@cern.ch>
2015-06-19 10:45:57 +0200

    also copy eos to rome

--- scripts/romaT2toEOS.py
+++ scripts/romaT2toEOS.py
@@ -13,12 +13,17 @@ OUTPUTDIR = options.OUTPUTDIR
 
 ins = open( INPUTLIST, "r" )
 for line in ins:
+    line.rstrip('\n')
+    #print ("%s" % line)
     #print ("%s" % line)
     head, tail = os.path.split(line)
     head = head.rstrip('\n')
     tail = tail.rstrip('\n')
     #print("%s" % tail)
-    cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s\" \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
+    ##rome to eos
+    #cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s\" \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
+    ##eos to rome
+    cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s\" \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
     print cmd    
     
     #check if file already exists
