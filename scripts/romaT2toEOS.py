347aed47b372c802e0c3b6b55fd33b9be45c2dca
Francesco Santanastasio <francesco.santanastasio@cern.ch>
2015-07-13 11:46:15 +0200

    added instructions

--- scripts/romaT2toEOS.py
+++ scripts/romaT2toEOS.py
@@ -2,9 +2,23 @@
 import os
 import optparse  
 
-# edit script depending if you copy from eos-to-rome or from rome-to-eos
+#-----------------------------------------------
+
+# 1) edit script depending if you copy from eos-to-rome or from rome-to-eos (see @--@ below)
+
+# 2) create input list
+
+ # a) example of input list from RomeT2
+ # ls /pnfs/roma1.infn.it/data/cms//store/user/gdimperi/Dijet/data/Run2015B_JetHT_12June2015DCSJson_JECV5_4422c9ba9d/JetHT/crab_JetHT__Run2015B-PromptReco-v1__MINIAOD/150712_211959/0000/ | awk '{ print "/pnfs/roma1.infn.it/data/cms//store/user/gdimperi/Dijet/data/Run2015B_JetHT_12June2015DCSJson_JECV5_4422c9ba9d/JetHT/crab_JetHT__Run2015B-PromptReco-v1__MINIAOD/150712_211959/0000/"$0}' > /tmp/list.txt
+
+ # b) example of input list from eos
+ # eos ls /eos/cms/store/group/phys_exotica/dijet/Dijet13TeV/santanas/Run2015B_JetHT_12June2015DCSJson_JECV5_4422c9ba9d_150712_211959/ | awk '{print "/eos/cms/store/group/phys_exotica/dijet/Dijet13TeV/santanas/Run2015B_JetHT_12June2015DCSJson_JECV5_4422c9ba9d_150712_211959/"$0}' > /tmp/list.txt
+
+# 3) start the copy
 # example:  python romaT2toEOS.py --inputList list.txt --outputDir /pnfs/roma1.infn.it/data/cms/store/user/santanas/rootTrees/Spring15_v1/
 #example of eos path: /eos/cms/store/group/phys_exotica/dijet/Dijet13TeV/juska/Spring15_JECV5/RSGravitonToQuarkQuark_kMpl01_M_9000_TuneCUETP8M1_13TeV_pythia8__RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1__MINIAODSIM_9.root
+
+#-----------------------------------------------
 
 usage = "usage: %prog [options]"
 parser = optparse.OptionParser(usage)
@@ -24,10 +38,14 @@ for line in ins:
     head = head.rstrip('\n')
     tail = tail.rstrip('\n')
     #print("%s" % tail)
-    ##rome to eos
-    #cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s\" \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
-    ##eos to rome
-    cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s\" \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
+
+#####################################################################33
+    ##rome to eos @--@
+    cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s\" \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
+    ##eos to rome @--@
+    #cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/%s\" \"srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/%s/%s\" " %  (line, OUTPUTDIR, tail)  )
+#####################################################################33
+
     print cmd    
     
     #check if file already exists
