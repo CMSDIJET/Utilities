65cd1786f6120a1c2c3ba81945634a9bc69cdd84
Francesco Santanastasio <francesco.santanastasio@cern.ch>
2016-01-14 16:24:58 +0100

    check if file exists

--- scripts/copyWithLcg.py
+++ scripts/copyWithLcg.py
@@ -3,6 +3,7 @@ import os
 import optparse 
 import sys 
 import subprocess
+import re
 
 usage = "usage: %prog [options]"
 parser = optparse.OptionParser(usage)
@@ -10,7 +11,6 @@ parser.add_option("--inputStorage",action="store",type="string",dest="INPUTSTORA
 parser.add_option("--outputStorage",action="store",type="string",dest="OUTPUTSTORAGE")
 parser.add_option("--inputDir",action="store",type="string",dest="INPUTDIR")
 parser.add_option("--outputDir",action="store",type="string",dest="OUTPUTDIR")
-
 
 (options, args) = parser.parse_args()
 INPUTSTORAGE = options.INPUTSTORAGE
@@ -21,18 +21,15 @@ OUTPUTDIR = options.OUTPUTDIR
 storagePath = {'ROME': "srm://cmsrm-se01.roma1.infn.it:8443/srm/managerv2?SFN=/", 
                'CERN': "srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/"}
 
-out = ()
-
 if INPUTSTORAGE == 'CERN':
-    print INPUTSTORAGE
+    #print INPUTSTORAGE
     proc = subprocess.Popen(["eos ls %s | grep root" % INPUTDIR], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
 if INPUTSTORAGE == 'ROME':
-    print INPUTSTORAGE
+    #print INPUTSTORAGE
     proc = subprocess.Popen(["ls %s | grep root" % INPUTDIR], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
 
-print "program output:", out
 out = out.splitlines()
 
 for infile in out:
@@ -43,5 +40,19 @@ for infile in out:
 
     ##rome to eos @--@
     cmd =  ("lcg-cp -b --vo cms -D srmv2 -U srmv2 -v \"%s%s\" \"%s%s/%s\" " %  (storagePath[INPUTSTORAGE],inpath,storagePath[OUTPUTSTORAGE],OUTPUTDIR,infile)  )
+    #print cmd    
 
-    print cmd    
+    #check if file already exists
+    checkfilecommand = "lcg-ls -b --vo cms -D srmv2 -v \"%s%s/%s\" " %  (storagePath[OUTPUTSTORAGE],OUTPUTDIR,infile)
+    proc = subprocess.Popen(["lcg-ls -b --vo cms -D srmv2 -v \"%s%s/%s\" " %  (storagePath[OUTPUTSTORAGE],OUTPUTDIR,infile)], stdout=subprocess.PIPE, shell=True)
+    (checkfile, err) = proc.communicate()
+    checkfile = checkfile.splitlines()
+    #print checkfile
+
+    if len(checkfile)==2:
+        print("WARNING: File \"%s%s/%s\" already exists in the destination! I'm not overwriting!" %  (storagePath[OUTPUTSTORAGE],OUTPUTDIR,infile))
+    if len(checkfile)==1:
+        print "I'll start the copy"
+        print cmd
+        os.system(cmd)
+
